ea2dbc84e94777eb069fa5ff61abe14967ad35e8
NISHANT7-cmd <nishantshewale69@gmail.com>
2026-06-26 15:44:28 +0530

    Fix import statements formatting in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import \"@/App.css\";
-import { BrowserRouter, Routes, Route } from \"react-router-dom\";
-import Header from \"@/components/Header\";
-import Footer from \"@/components/Footer\";
-import Home from \"@/pages/Home\";
-import Properties from \"@/pages/Properties\";
-import PropertyDetail from \"@/pages/PropertyDetail\";
-import Valuation from \"@/pages/Valuation\";
-import About from \"@/pages/About\";
-import Contact from \"@/pages/Contact\";
+import \"@/App.css";
+import { BrowserRouter, Routes, Route } from \"react-router-dom";
+import Header from \"@/components/Header";
+import Footer from \"@/components/Footer";
+import Home from \"@/pages/Home";
+import Properties from \"@/pages/Properties";
+import PropertyDetail from \"@/pages/PropertyDetail";
+import Valuation from \"@/pages/Valuation";
+import About from \"@/pages/About";
+import Contact from \"@/pages/Contact";
 
 function ScrollToTop() {
   // Handled by react-router scroll restoration default; minimal stub
